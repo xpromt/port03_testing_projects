a36ecf81307236287668dabb5446615313f3bfbd
unknown <xpromt@gmail.com>
2022-05-25 12:50:16 +0300

    Routes added start

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,34 +1,25 @@
+import { Link } from "react-router-dom";
 import logo from '../../logo.svg';
+
 import './Header.css';
 
 const Header = () => {
   return (
     <header className="App-header">
-    <img className="App-header__logo" src={logo} alt="logo" />
-    <a
-      className="App-link App-header__item"
-      href="/"
-      target="_blank"
-      rel="noopener noreferrer"
-    >
-    Box
-    </a>
-    <a
-      className="App-link App-header__item"
-      href="/"
-      target="_blank"
-      rel="noopener noreferrer"
-    >
-    Click
-    </a>
-    <a
-      className="App-link App-header__item"
-      href="/"
-      target="_blank"
-      rel="noopener noreferrer"
-    >
-    RComponent
-    </a>
+      <nav>
+        <img className="App-header__logo" src={logo} alt="logo" />
+        <Link to="first">First</Link>
+        <Link to="click"
+          className="App-link App-header__item"
+        >
+        Click
+        </Link>
+        <Link to="rcomponent"
+          className="App-link App-header__item"
+        >
+        RComponent
+        </Link>
+      </nav>
   </header>
   );
 }
